c29cd93b55b02d64f03e8341c0cd4f5c69e6a495
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2013-02-21 02:26:35 +0800

    added route

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,5 +5,6 @@ angular.module('kaching', []).
       when('/monthly.html', {templateUrl: 'partials/monthly.html',   controller: MainLayoutCtrl}).
       when('/gallery.html', {templateUrl: 'partials/gallery.html',   controller: MainLayoutCtrl}).
       when('/scan_receipt.html', {templateUrl: 'partials/scan_receipt.html',   controller: MainLayoutCtrl}).
+      when('/purchase_summary.html', {templateUrl: 'partials/purchase_summary.html',   controller: MainLayoutCtrl}).
       otherwise({redirectTo: '/'});
 }]);
