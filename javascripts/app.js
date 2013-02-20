4f0faca88f050cab0c45d37411101aa7360589ef
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2013-02-21 00:58:58 +0800

    asd

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,7 @@
 angular.module('kaching', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/', {templateUrl: 'partials/home.html',   controller: HomeDetailCtrl}).
-      when('/monthly.html', {templateUrl: 'partials/monthly.html',   controller: HomeDetailCtrl}).
+      when('/', {templateUrl: 'partials/home.html',   controller: MainLayoutCtrl}).
+      when('/monthly.html', {templateUrl: 'partials/monthly.html',   controller: MainLayoutCtrl}).
       otherwise({redirectTo: '/'});
 }]);
--- javascripts/controllers.js
+++
@@ -1,3 +0,0 @@
-function HomeDetailCtrl($scope, $routeParams) {
-  $scope.phoneId = $routeParams.phoneId;
-}
