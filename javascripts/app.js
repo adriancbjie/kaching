561d73f1396d468e345a3a0df816ba6ea3b88b43
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2013-02-21 00:47:09 +0800

    asd

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,7 +2,7 @@ angular.module('kaching', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'partials/home.html',   controller: HomeDetailCtrl}).
-      when('/kaching/monthly.html', {templateUrl: 'partials/monthly.html',   controller: HomeDetailCtrl}).
+      when('/monthly.html', {templateUrl: 'partials/monthly.html',   controller: HomeDetailCtrl}).
       // when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
       otherwise({redirectTo: '/'});
 }]);
