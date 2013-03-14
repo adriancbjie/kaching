188bf3e1a68abdb9b330782b8d441826b6511023
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2013-03-14 14:59:33 +0800

    asd

--- javascripts/app.js
+++ javascripts/app.js
@@ -17,6 +17,8 @@ app.run(function ($rootScope) {
 		'monthly_budget': 200.00
 	};
 
+	$rootScope.asd=[7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6];
+
 	$rootScope.photo_lib = 
 	[{
 		'url' : 'https://dl.dropbox.com/u/3179945/kaching/Single%20%28105%20x%20222%29/S1.png'
